b48fff967ea4efe2f969daceef441ebfbb1b6a10
j-sebas-29 <jsebas981029@gmail.com>
2024-06-17 19:46:36 -0500

    avance 1

---
+++ M03 JavaScript Fundamentos/clase-3.js
@@ -0,0 +1,18 @@
+/* Variables*/
+
+//string
+var cadena = "henry"; //tipo de palabras
+
+//numeros
+
+var numero = 10; //numeros
+//booleanos
+var bool = true; //dato de verdadero o falso
+
+//undifined
+
+var undi = undefined; // dato no definido 
+
+//null 
+
+var n = null; // el progrador define el valor null (vacio)
